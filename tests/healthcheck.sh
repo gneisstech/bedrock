7e81bc9273aeb2cf26db944857ccc142aac20106
Paul Charlton <techguru@byiq.com>
2019-09-19 10:02:00 -0400

    initial commit for bytelight devops

---
+++ tests/healthcheck.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+# usage: docker_install.sh
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Environment Variables
+# ---------------------
+
+# Arguments
+# ---------------------
+
+function dev_site_url () {
+  echo 'https://cf.dev.atrius-iot.com'
+  #echo 'http://atgcfexp-post-auth-pip.southcentralus.cloudapp.azure.com'
+}
+
+function site_token () {
+  az account get-access-token 2>/dev/null | jq -r '.accessToken'
+}
+
+function curl_auth () {
+  local -r theURL="${1}"
+  curl -sS -H "Authorization: Bearer $(site_token)" "${theURL}" --fail -w "%{http_code}: " -o /dev/null 2>/dev/null || echo "Unauthorized"
+}
+
+function check_main () {
+  echo "=== begin checking root URL of site"
+  curl -sS "$(dev_site_url)"
+  echo
+  echo "=== done checking root URL of site"
+}
+
+function check_auth_proxy () {
+  echo "=== begin checking auth_proxy of site"
+  curl -sS "$(dev_site_url)/robots.txt"
+  echo
+  echo "=== done checking auth_proxy of site"
+}
+
+function check_app () {
+  echo "=== begin checking app URL of site"
+  curl_auth "$(dev_site_url)/cf-app/"
+  echo "=== done checking app URL of site"
+}
+
+function check_admin_api () {
+  echo "=== begin checking admin_api URL of site"
+  curl_auth "$(dev_site_url)/cf-admin"
+  echo "=== begin checking admin_api URL of site"
+}
+
+function check_ingest_api () {
+  echo "=== begin checking ingest_api URL of site"
+  curl_auth "$(dev_site_url)/cf-ingest"
+  echo "=== begin checking ingest_api URL of site"
+}
+
+function check_visualization_api () {
+  echo "=== begin checking visualization_api URL of site"
+  curl_auth "$(dev_site_url)/cf-visualization"
+  echo "=== begin checking visualization_api URL of site"
+}
+
+function healthcheck () {
+  check_main
+  check_auth_proxy
+  check_app
+  check_admin_api
+  check_ingest_api
+  check_visualization_api
+}
+
+healthcheck
