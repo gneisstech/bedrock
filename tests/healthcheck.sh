a78f81676a2c4be7301da49f9fd4fd495970fead
Paul Charlton <techguru@byiq.com>
2019-10-16 21:09:34 -1000

    update location of predefined resource groups

--- tests/healthcheck.sh
+++ tests/healthcheck.sh
@@ -17,7 +17,7 @@ set -o pipefail
 # ---------------------
 
 function dev_site_url () {
-  echo 'https://cf-staging.dev.atrius-iot.com'
+  echo 'https://cf-qa01.dev.atrius-iot.com'
   #echo 'http://atgcfexp-post-auth-pip.southcentralus.cloudapp.azure.com'
 }
 
