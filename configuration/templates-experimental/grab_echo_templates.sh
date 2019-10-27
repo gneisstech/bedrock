639f8eb85e6a7b3ac5e204598d409cdf6db549d0
Paul Charlton <techguru@byiq.com>
2019-10-26 20:12:23 -1000

    collect more telemetry on logging for web application service to enable automation of logging configuration

---
+++ configuration/templates-experimental/grab_echo_templates.sh
@@ -0,0 +1,5 @@
+az resource show --resource-group Web-CfDev --name http-echo-debugging --resource-type Microsoft.Web/sites > http-echo-debugging.logging.json
+az webapp config appsettings list --resource-group Web-CfDev --name http-echo-debugging > http-echo-debugging.config.logging.json
+az webapp config container show --resource-group Web-CfDev --name http-echo-debugging > http-echo-debugging.container-settings.logging.json
+az webapp config connection-string list --resource-group Web-CfDev --name http-echo-debugging > http-echo-debugging.connection-strings.logging.json
+az webapp log show --resource-group Web-CfDev --name http-echo-debugging > http-echo-debugging.logging.logging.json
