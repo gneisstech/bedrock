e567172467c6dd209f1235d1b3f7a04ca3cd8210
mystic knight <techguru@byiq.com>
2020-03-09 20:12:45 -0400

    add logic for promoting elm containers

--- recipes/promote_containers.sh
+++ recipes/promote_containers.sh
@@ -28,15 +28,16 @@ function acr_logins () {
 
 function promote_containers () {
     local -r originRepo='cfdevregistry.azurecr.io'
-    local -r containers='cf-oauth-proxy-docker cf-react-app-docker cf-ruby-api-docker cf-self-healing-api-docker cf-self-healing-app-docker'
+    #local -r containers='cf-oauth-proxy-docker cf-react-app-docker cf-ruby-api-docker cf-self-healing-api-docker cf-self-healing-app-docker'
     #local -r containers='cf-self-healing-api-docker cf-self-healing-app-docker'
+    local -r containers='cf-admin-web-api-docker cf-atrius-objects-api-docker cf-authz-web-api-docker cf-elm-web-api-docker cf-network-view-web-api-docker cf-oauth-proxy-docker cf-react-app-docker'
     acr_logins
     for image in ${containers}; do
         local imageWithTag originPath targetPath targetRepo
         imageWithTag="${image}:connected-facilities"
         originPath="${originRepo}/${imageWithTag}"
         docker pull "${originPath}"
-        for targetRepo in cfstagingregistry cfqaregistry; do
+        for targetRepo in cfqaregistry; do
             local targetPath
             targetPath="${targetRepo}.azurecr.io/${imageWithTag}"
             docker tag "${originPath}" "${targetPath}"
