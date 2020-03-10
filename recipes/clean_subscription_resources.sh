edda2e887983e2140e2f30515712261bc1695f38
mystic knight <techguru@byiq.com>
2020-03-09 20:11:54 -0400

    create script to clean transient subscription resources (leave the persistent ones)

---
+++ recipes/clean_subscription_resources.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+# usage: clean_subscription_resources.sh
+#
+# the intent of this script is to remove all non-persistent resources from a subscription
+#
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Environment Variables
+# ---------------------
+declare -r CLEAN_SUBSCRIPTION="${CLEAN_SUBSCRIPTION:-ConnectedFacilities-Dev}"
+declare -r CLEAN_RESOURCE_GROUPS_SUFFIX="${CLEAN_RESOURCE_GROUPS_SUFFIX:--exp-cfdev}"
+
+# Arguments
+# ---------------------
+
+function repo_root () {
+    git rev-parse --show-toplevel
+}
+
+function cleaningSubscription () {
+    printf "%s" "${CLEAN_SUBSCRIPTION}"
+}
+
+function cleaningGroupsSuffix () {
+    printf "%s" "${CLEAN_RESOURCE_GROUPS_SUFFIX}"
+}
+
+function get_resource_ids_for_groups () {
+    local -r groupSuffix="$(cleaningGroupsSuffix)"
+    az resource list --subscription "$(cleaningSubscription)" | jq -r '.[].id' | grep -i -- "${groupSuffix}\/"
+}
+
+function remove_persistent_resources () {
+    egrep -vi 'Microsoft.KeyVault|Microsoft.Sql|publicIPAddresses|Microsoft.Storage|\/ingestion|\/Acr-'
+}
+
+function clean_resource_ids () {
+    xargs -P 32 az resource delete --ids
+}
+
+function clean_subscription_resources () {
+   get_resource_ids_for_groups | remove_persistent_resources | tee /dev/stderr | clean_resource_ids
+}
+
+clean_subscription_resources
