54812880aa410484d020b90380b3276833dde680
Abou-Rizk, Mitri J <mitri@bytelight.com>
2020-03-11 20:18:46 +0000

    Updated bless_development_artifacts.sh. Take values from caller if available

--- ci/recipes/bless_development_artifacts.sh
+++ ci/recipes/bless_development_artifacts.sh
@@ -22,9 +22,9 @@ declare -rx ORIGIN_SUBSCRIPTION="${ORIGIN_SUBSCRIPTION:-ConnectedFacilities-Dev}
 declare -rx ORIGIN_REPOSITORY="${ORIGIN_REPOSITORY:-cfdevregistry}"
 declare -rx ORIGIN_RESOURCE_PREFIX="${ORIGIN_RESOURCE_PREFIX:-cf-dev-}"
 declare -rx TARGET_REPOSITORY="${TARGET_REPOSITORY:-cfqaregistry}"
-declare -rx RELEASE_PREFIX='r'
-declare -rx DEFAULT_RELEASE='r0.0.0'
-declare -rx BUMP_SEMVER="true"
+declare -rx RELEASE_PREFIX='${RELEASE_PREFIX}:-r'
+declare -rx DEFAULT_RELEASE='${DEFAULT_RELEASE}:-r0.0.0'
+declare -rx BUMP_SEMVER="${BUMP_SEMVER}:-true"
 
 function repo_root () {
     git rev-parse --show-toplevel
