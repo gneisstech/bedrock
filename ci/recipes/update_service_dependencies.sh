7c032d10395b620f51ff041ed347667e5117abeb
mystic knight <techguru@byiq.com>
2020-05-23 08:54:28 -1000

    initial stubs for automated update of umbrella chart

---
+++ ci/recipes/update_service_dependencies.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+# usage: update_service_dependencies.sh
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Environment Variables
+# ---------------------
+declare -rx BUILD_SOURCEBRANCH
+
+# Arguments
+# ---------------------
+
+declare -rx ORIGIN_ENVIRONMENT="${ORIGIN_ENVIRONMENT:-dev}"
+declare -rx ORIGIN_REPOSITORY="${ORIGIN_REPOSITORY:-cfdevregistry}"
+declare -rx RELEASE_PREFIX="${RELEASE_PREFIX:-r}"
+declare -rx DEFAULT_SEMVER="${DEFAULT_SEMVER:-0.0.0}"
+declare -rx BUMP_SEMVER="${BUMP_SEMVER:-true}"
+declare -rx BUILD_REPOSITORY_LOCALPATH="${BUILD_REPOSITORY_LOCALPATH:-.}"
+declare -rx required_repo_branch="${required_repo_branch:-master-bytelight}"
+
+function repo_root () {
+    git rev-parse --show-toplevel
+}
+
+function current_repo_branch () {
+    git status -b  | grep "^On branch" | sed -e 's/.* //'
+}
+
+function validate_branch () {
+    printf 'branches: required [%s], current [%s], build_sourcebranch [%s]\n' \
+        "${required_repo_branch}" \
+        "$(current_repo_branch)" \
+        "${BUILD_SOURCEBRANCH}"
+##    [[ "$(current_repo_branch)" == "${required_repo_branch}" ]] \
+##        || [[ "${BUILD_SOURCEBRANCH:-}" == "refs/heads/${required_repo_branch}" ]]
+}
+
+function services_changed_semver () {
+    printf 'triggered build from [%s]' "${BUILD_SOURCEBRANCH}"
+    env
+    true
+}
+
+function update_service_dependencies () {
+    if validate_branch; then
+        if services_changed_semver; then
+            $(repo_root)/ci/recipes/update_umbrella_chart.sh
+        fi
+    fi
+}
+
+update_service_dependencies
---
+++ ci/recipes/update_umbrella_chart.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+# usage: update_service_dependencies.sh
+
+# Exit script if you try to use an uninitialized variable.
+set -o nounset
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# Environment Variables
+# ---------------------
+declare -rx BUILD_SOURCEBRANCH
+
+# Arguments
+# ---------------------
+
+declare -rx ORIGIN_ENVIRONMENT="${ORIGIN_ENVIRONMENT:-dev}"
+declare -rx ORIGIN_REPOSITORY="${ORIGIN_REPOSITORY:-cfdevregistry}"
+declare -rx RELEASE_PREFIX="${RELEASE_PREFIX:-r}"
+declare -rx DEFAULT_SEMVER="${DEFAULT_SEMVER:-0.0.0}"
+declare -rx BUMP_SEMVER="${BUMP_SEMVER:-true}"
+declare -rx BUILD_REPOSITORY_LOCALPATH="${BUILD_REPOSITORY_LOCALPATH:-.}"
+
+function repo_root () {
+    git rev-parse --show-toplevel
+}
+
+function update_semver () {
+    printf 'triggered build from [%s]' "${BUILD_SOURCEBRANCH}"
+    env
+}
+
+function current_repo_branch () {
+    git status -b  | grep "^On branch" | sed -e 's/.* //'
+}
+
+function validate_branch () {
+    [[ "$(current_repo_branch)" == "${required_repo_branch}" ]] \
+        || [[ "${BUILD_SOURCEBRANCH:-}" == "refs/heads/${required_repo_branch}" ]]
+}
+
+function update_umbrella_chart () {
+    if validate_branch; then
+        update_semver
+    fi
+}
+
+update_service_dependencies.sh
