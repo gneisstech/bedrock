7a373666bb29dd45af3749479d0338d2cfd94c3e
mystic knight <techguru@byiq.com>
2020-05-23 09:17:25 -1000

    initial stubs for automated update of umbrella chart

--- ci/recipes/update_umbrella_chart.sh
+++ ci/recipes/update_umbrella_chart.sh
@@ -23,12 +23,13 @@ declare -rx RELEASE_PREFIX="${RELEASE_PREFIX:-r}"
 declare -rx DEFAULT_SEMVER="${DEFAULT_SEMVER:-0.0.0}"
 declare -rx BUMP_SEMVER="${BUMP_SEMVER:-true}"
 declare -rx BUILD_REPOSITORY_LOCALPATH="${BUILD_REPOSITORY_LOCALPATH:-.}"
+declare -rx required_repo_branch="${required_repo_branch:-master-bytelight}"
 
 function repo_root () {
     git rev-parse --show-toplevel
 }
 
-function update_semver () {
+function update_umbrella_semver () {
     printf 'triggered build from [%s]' "${BUILD_SOURCEBRANCH}"
     env
 }
@@ -38,13 +39,17 @@ function current_repo_branch () {
 }
 
 function validate_branch () {
-    [[ "$(current_repo_branch)" == "${required_repo_branch}" ]] \
-        || [[ "${BUILD_SOURCEBRANCH:-}" == "refs/heads/${required_repo_branch}" ]]
+    printf 'branches: required [%s], current [%s], build_sourcebranch [%s]\n' \
+        "${required_repo_branch}" \
+        "$(current_repo_branch)" \
+        "${BUILD_SOURCEBRANCH}"
+##    [[ "$(current_repo_branch)" == "${required_repo_branch}" ]] \
+##        || [[ "${BUILD_SOURCEBRANCH:-}" == "refs/heads/${required_repo_branch}" ]]
 }
 
 function update_umbrella_chart () {
     if validate_branch; then
-        update_semver
+        update_umbrella_semver
     fi
 }
 
