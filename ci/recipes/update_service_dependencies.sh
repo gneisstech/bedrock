19acce7255ec554cd88865de4c2001c4deb7d868
mystic knight <techguru@byiq.com>
2020-05-24 07:29:13 -1000

    initial stubs for automated update of umbrella chart

--- ci/recipes/update_service_dependencies.sh
+++ ci/recipes/update_service_dependencies.sh
@@ -22,10 +22,7 @@ declare -rx TF_BUILD
 declare -rx ORIGIN_ENVIRONMENT="${ORIGIN_ENVIRONMENT:-dev}"
 declare -rx ORIGIN_REPOSITORY="${ORIGIN_REPOSITORY:-cfdevregistry}"
 declare -rx RELEASE_PREFIX="${RELEASE_PREFIX:-r}"
-declare -rx DEFAULT_SEMVER="${DEFAULT_SEMVER:-0.0.0}"
-declare -rx BUMP_SEMVER="${BUMP_SEMVER:-true}"
 declare -rx BUILD_REPOSITORY_LOCALPATH="${BUILD_REPOSITORY_LOCALPATH:-.}"
-declare -rx required_repo_branch="${required_repo_branch:-master-bytelight}"
 
 function repo_root () {
     git rev-parse --show-toplevel
@@ -35,38 +32,46 @@ function is_azure_pipeline_build () {
     [[ "True" == "${TF_BUILD:-}" ]]
 }
 
+function git_repo_branch () {
+    git rev-parse --abbrev-ref HEAD
+}
+
 function current_repo_branch () {
     local branch
     if is_azure_pipeline_build; then
         branch="${BUILD_SOURCEBRANCHNAME:-}"
     else
-        branch="$(git rev-parse --abbrev-ref HEAD)"
+        branch="$(git_repo_branch)"
     fi
     printf "%s" "${branch}"
 }
 
-function validate_branch () {
+function trace_environment () {
+    printf 'triggered build from [%s]' "${BUILD_SOURCEBRANCH:-}\n"
     git status
-    printf 'branches: required [%s], current [%s], build_sourcebranch [%s]\n' \
-        "${required_repo_branch}" \
+    printf 'branches: current [%s], build_sourcebranch [%s], git branch [%s]\n' \
         "$(current_repo_branch)" \
-        "${BUILD_SOURCEBRANCH:-}"
-    [[ "$(current_repo_branch)" == "${required_repo_branch}" ]] \
-        || [[ "${BUILD_SOURCEBRANCH:-}" == "refs/heads/${required_repo_branch}" ]] \
-        || printf 'not on required branch\n'
+        "${BUILD_SOURCEBRANCH:-}" \
+        "$(git_repo_branch)"
     env
+}
+
+#
+# there are four basic cases to consider:
+# chart.lock semver matches latest service semver => no change
+# chart.lock semver does not match latest service semver => remove lock, rebuild umbrella [inspect service semver for new feature or breaking change, respond accordingly]
+# chart.lock semver does not exist for service => remove lock, rebuild umbrella [ new feature change ]
+# chart.lock semver exists, but no artifacts for service => remove lock, rebuild umbrella [breaking change]
+#
+
+function services_changed_semver () {
     true
 }
 
-function services_changed_semver () {
-    printf 'triggered build from [%s]' "${BUILD_SOURCEBRANCH:-}\n"
-}
-
 function update_service_dependencies () {
-    if validate_branch; then
-        if services_changed_semver; then
-            $(repo_root)/ci/recipes/update_umbrella_chart.sh
-        fi
+    trace_environment
+    if services_changed_semver; then
+        $(repo_root)/ci/recipes/update_umbrella_chart.sh
     fi
 }
 
