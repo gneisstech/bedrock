ebff1233314fa72f71f3643f43dfb921de493774
mystic knight <techguru@byiq.com>
2020-05-24 22:24:25 -1000

    initial stubs for automated update of umbrella chart

--- ci/recipes/update_service_dependencies.sh
+++ ci/recipes/update_service_dependencies.sh
@@ -62,6 +62,14 @@ function get_upstream_services () {
         | sort -u
 }
 
+function pipeline_as_json () {
+    yq -r --tojson "$(repo_root)/ci/pipelines/update_service_dependencies.yml"
+}
+
+function get_pipeline_services () {
+    pipeline_as_json | jq -r -e '.resources.pipelines[].pipeline' | sort -u
+}
+
 function chart_dir () {
     printf '%s/configuration/k8s/charts/cf-deployment-umbrella/' "$(repo_root)"
 }
@@ -95,22 +103,94 @@ function get_helm_services () {
     jq -r '.[].name' | sed -e 's|.*/||' -e 's|"$||' | sort -u
 }
 
-function services_changed_semver () {
-    local upstream_services chart_services locked_chart_services helm_services_json helm_services
+function services_are_subset () {
+    # return true if lhs is a subset of rhs
+    local -r lhs="${1}"
+    local -r rhs="${2}"
+    ! (diff <(printf '%s' "${lhs}") <(printf '%s' "${rhs}") | grep '^<')
+}
+
+function semver_breaking_change () {
+    printf 'BREAKING CHANGE DETECTED\n'
+    true
+}
+
+function semver_new_feature () {
+    printf 'NEW FEATURE DETECTED\n'
+    true
+}
+
+function has_breaking_changes () {
+    false
+}
+
+function has_new_features () {
+    false
+}
+
+function update_semver () {
+    local locked_chart_services="${1}"
+    local chart_services="${2}"
+    local helm_services="${3}"
+
+    if ! services_are_subset "${locked_chart_services}" "${chart_services}"; then
+        # if locked_chart_services contains any service not in the chart, then breaking change
+        semver_breaking_change
+        return
+    fi
+
+    if services_are_subset "${locked_chart_services}" "${helm_services}"; then
+        if has_breaking_changes "${locked_chart_services}" "${helm_services}"; then
+            # if any service has a breaking change, then overall breaking change
+            semver_breaking_change
+            return
+        fi
+        if has_new_features "${locked_chart_services}" "${helm_services}"; then
+            # if any service has a new feature, then overall new feature
+            semver_new_feature
+            return
+        fi
+    fi
+
+    if ! services_are_subset "${chart_services}" "${locked_chart_services}"; then
+        # if chart_services contains any service not in the locked_chart_services, then new feature
+        semver_new_feature
+        return
+    fi
+}
+
+function check_services_config () {
+    local upstream_services pipeline_services
+    local chart_services locked_chart_services
+    local helm_services_json helm_services
     upstream_services="$(get_upstream_services)"
+    pipeline_services="$(get_pipeline_services)"
     chart_services="$(get_chart_services)"
     locked_chart_services="$(get_locked_chart_services)"
     helm_services_json="$(get_helm_services_json)"
     helm_services="$(get_helm_services <<< "${helm_services_json}")"
     printf 'upstream [%s]\n\n' "${upstream_services}"
+    printf 'pipeline [%s]\n\n' "${pipeline_services}"
     printf 'chart [%s]\n\n' "${chart_services}"
     printf 'locked chart [%s]\n\n' "${locked_chart_services}"
     printf 'helm [%s]\n\n' "${helm_services}"
+    if [[ "${pipeline_services}" != "${chart_services}" ]] ; then
+        printf 'ERROR: misconfigured repository: upstream does not match update_service_dependencies.yml\n'
+    elif [[ "${upstream_services}" != "${chart_services}" ]]; then
+        printf 'ERROR: misconfigured repository: upstream does not match Chart.yaml\n'
+    elif services_are_subset "${chart_services}" "${helm_services}"; then
+        # if chart_services contains any service not in the helm_services, then ERROR
+        printf 'ERROR: Chart.yaml refers to unpublished services\n'
+    else
+        update_semver "${locked_chart_services}" "${chart_services}" "${helm_services}"
+        return
+    fi
+    false
 }
 
 function update_service_dependencies () {
     update_helm_repo
-    if services_changed_semver; then
+    if check_services_config; then
         # shellcheck disable=2046
         $(repo_root)/ci/recipes/update_umbrella_chart.sh
     fi
