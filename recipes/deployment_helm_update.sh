ced4dca61ca9e903860385ace91e9d9ee6e829b5
mystic knight <techguru@byiq.com>
2020-06-30 16:10:01 -1000

    remove helm dry run

--- recipes/deployment_helm_update.sh
+++ recipes/deployment_helm_update.sh
@@ -239,16 +239,6 @@ function update_helm_chart_on_k8s () {
             "$(get_helm_deployment_name "${deployment_json}" )" \
             "${registry}/${chart_name}" \
             --version "$(get_helm_version "${deployment_json}")" \
-            --dry-run \
-            --values <(cat <<< "${helm_values}")
-        helm upgrade \
-            --install \
-            --kube-context "$(get_kube_context "${deployment_json}")" \
-            --namespace "$(get_kube_namespace "${deployment_json}")" \
-            --history-max 200 \
-            "$(get_helm_deployment_name "${deployment_json}" )" \
-            "${registry}/${chart_name}" \
-            --version "$(get_helm_version "${deployment_json}")" \
             --timeout "$(get_migration_timeout "${deployment_json}")" \
             --wait \
             --values <(cat <<< "${helm_values}")
