9a22068c57e1a070ed09ad166b7a5f32287d7795
mystic knight <techguru@byiq.com>
2020-10-15 19:33:53 -1000

    add automation for datadog deployment

--- paas/recipes/deploy_datadog.sh
+++ paas/recipes/deploy_datadog.sh
@@ -30,6 +30,25 @@ function get_deployment_json_by_name () {
 
 function deploy_datadog () {
     local -r deployment_name="${1}"
+    local deployment_json datadog_enabled
+    deployment_json="$(get_deployment_json_by_name "${deployment_name}")"
+    datadog_enabled="$(jq -r -e '.paas.datadog.enabled' <<< "${deployment_json}")"
+    if [[ "${datadog_enabled}" == 'true' ]]; then
+        local datadog_namespace k8s_context
+        datadog_namespace="$(jq -r -e '.paas.datadog.namespace' <<< "${deployment_json}")"
+        k8s_context="$(jq -r -e '.k8s.context' <<< "${deployment_json}")"
+        kubectl --context "${k8s_context}" create ns "${datadog_namespace}" 2> /dev/null || true
+        jq -r -e '.paas.datadog.values' <<< "${deployment_json}"
+        helm upgrade --install \
+            --kube-context "${k8s_context}" \
+            --namespace "${datadog_namespace}" \
+            --history-max 20 \
+            'datadog' \
+            "$(repo_root)/configuration/k8s/charts/datadog/" \
+            --timeout "10m" \
+            --wait \
+            --values <(jq -r -e '.paas.datadog.values' <<< "${deployment_json}")
+    fi
 }
 
 deploy_datadog "${@}"
