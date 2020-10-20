fe35010c7c33b085ac8a59e41751659aeee22dfb
mystic knight <techguru@byiq.com>
2020-10-20 10:41:01 -1000

    check point gitlab solution

--- recipes/deployment_helm_ensure_persistent_volumes.sh
+++ recipes/deployment_helm_ensure_persistent_volumes.sh
@@ -109,12 +109,15 @@ function get_sa_key () {
     az storage account keys list --account-name "${sa_name}" | jq -r -e '.[0].value | @base64'
 }
 
-function create_azure_secret_resource () {
+function create_azure_file_volume_secret_resource () {
     local -r deployment_json="${1}"
     local -r sa_name="${2}"
     local -r sa_key="${3}"
     local -r volume_name="${4}"
     local -r volume_prefix="${5}"
+
+printf "\n\n===[%s]===\n\n" "${sa_name}" > /dev/stderr
+printf "\n\n===[%s]===\n\n" "$(echo -n "${sa_name}" | base64)" > /dev/stderr
 
 cat <<EOF
 apiVersion: v1
@@ -128,13 +131,13 @@ metadata:
   name: ${volume_prefix}-az-files-${volume_name}-state
   namespace: $(get_pv_secret_namespace "${deployment_json}")
 data:
-  azurestorageaccountname: '$(base64 <<< "${sa_name}")'
+  azurestorageaccountname: '$(echo -n "${sa_name}" | base64)'
   azurestorageaccountkey: '${sa_key}'
 
 EOF
 }
 
-function create_azure_volume_secret () {
+function create_azure_file_volume_secret () {
     local -r deployment_json="${1}"
     local -r sa_name="${2}"
     local -r volume_name="${3}"
@@ -146,7 +149,7 @@ function create_azure_volume_secret () {
         --context "$(get_kube_context "${deployment_json}")" \
         --namespace "$(get_pv_secret_namespace "${deployment_json}")" \
         apply -f <( \
-          create_azure_secret_resource \
+          create_azure_file_volume_secret_resource \
             "${deployment_json}" \
             "${sa_name}" \
             "${sa_key}" \
@@ -155,7 +158,7 @@ function create_azure_volume_secret () {
         )
 }
 
-function create_k8s_persistent_volume_resource () {
+function create_k8s_persistent_file_volume_resource () {
     local -r deployment_json="${1}"
     local -r volume_name="${2}"
     local -r volume_prefix="${3}"
@@ -175,22 +178,53 @@ spec:
     - ReadWriteMany
   persistentVolumeReclaimPolicy: Retain
   storageClassName: 'manual-${volume_name}'
-  mountOptions:
-    - dir_mode=0777
-    - file_mode=0777
-    - uid=0
-    - gid=0
-    - mfsymlinks
-    - cache=strict
   azureFile:
     shareName: ${volume_name}-state
     secretName: ${volume_prefix}-az-files-${volume_name}-state
     secretNamespace: $(get_pv_secret_namespace "${deployment_json}")
     readOnly: false
+  mountOptions:
+    - mfsymlinks
+EOF
+
+cat > /dev/null <<EOF2
+  xx-mountOptions:
+    - dir_mode=0777
+    - file_mode=0777
+    - uid=0
+    - gid=0
+    - cache=strict
+    - mfsymlinks
+EOF2
+}
+
+function create_k8s_persistent_file_volume_claim () {
+    local -r deployment_json="${1}"
+    local -r volume_name="${2}"
+    local -r volume_prefix="${3}"
+    local -r volume_quota="${4}"
+
+cat <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: ${volume_prefix}-az-files-${volume_name}-state-$(get_kube_namespace "${deployment_json}")-pvc
+  namespace: $(get_kube_namespace "${deployment_json}")
+  labels:
+    usage: ${volume_prefix}-az-files-${volume_name}-state-pvc
+spec:
+  resources:
+    requests:
+      storage: ${volume_quota}Gi
+  accessModes:
+    - ReadWriteMany
+  storageClassName: 'manual-${volume_name}'
+  volumeMode: 'Filesystem'
+  volumeName: ${volume_prefix}-az-files-${volume_name}-state-$(get_kube_namespace "${deployment_json}")-pv
 EOF
 }
 
-function create_k8s_persistent_volume () {
+function create_k8s_persistent_file_volume () {
     local -r deployment_json="${1}"
     local -r volume_name="${2}"
     local -r volume_prefix="${3}"
@@ -200,7 +234,17 @@ function create_k8s_persistent_volume () {
         --context "$(get_kube_context "${deployment_json}")" \
         --namespace "$(get_kube_namespace "${deployment_json}")" \
         apply -f <( \
-          create_k8s_persistent_volume_resource \
+          create_k8s_persistent_file_volume_resource \
+            "${deployment_json}" \
+            "${volume_name}" \
+            "${volume_prefix}" \
+            "${volume_quota}" \
+        )
+    kubectl \
+        --context "$(get_kube_context "${deployment_json}")" \
+        --namespace "$(get_kube_namespace "${deployment_json}")" \
+        apply -f <( \
+          create_k8s_persistent_file_volume_claim \
             "${deployment_json}" \
             "${volume_name}" \
             "${volume_prefix}" \
@@ -216,6 +260,14 @@ function get_deployment_json_by_name () {
             '.deployments[] | select(.name == "\($deployment_name)")'
 }
 
+function get_file_volume_metadata_by_name () {
+    local -r cluster_config_json="${1}"
+    local -r volume_name="${2}"
+    jq -r -e \
+      --arg volume_name "${volume_name}" \
+      '.target.paas.storage.azure_files[] | select(.name == "\($volume_name)")' <<< "${cluster_config_json}"
+}
+
 function create_artifacts_for_pv_az_file_volume () {
     local -r deployment_json="${1}"
     local -r volume_metadata="${2}"
@@ -225,12 +277,12 @@ function create_artifacts_for_pv_az_file_volume () {
     volume_prefix="$(get_pv_secret_namespace_prefix "${deployment_json}")"
     sa_name="$(jq -r -e '.storage_account_name' <<< "${volume_metadata}")"
 
-    create_azure_volume_secret \
+    create_azure_file_volume_secret \
       "${deployment_json}" \
       "${sa_name}" \
       "${volume_name}" \
       "${volume_prefix}"
-    create_k8s_persistent_volume \
+    create_k8s_persistent_file_volume \
       "${deployment_json}" \
       "${volume_name}" \
       "${volume_prefix}" \
@@ -238,23 +290,126 @@ function create_artifacts_for_pv_az_file_volume () {
     || true
 }
 
-function get_volume_metadata_by_name () {
-    local -r cluster_config_json="${1}"
-    local -r volume_name="${2}"
-    jq -r -e \
-      --arg volume_name "${volume_name}" \
-      '.target.paas.storage.azure_files[] | select(.name == "\($volume_name)")' <<< "${cluster_config_json}"
-}
-
 function iterate_az_file_volumes () {
     local -r deployment_json="${1}"
     local cluster_config_json volumes
     cluster_config_json="$(get_cluster_config_json "${deployment_json}" )"
-    volumes="$(jq -r -e '.target.paas.storage.azure_files[].name' <<< "${cluster_config_json}")"
+    volumes="$(jq -r '.target.paas.storage.azure_files[]? | .name // empty' <<< "${cluster_config_json}")"
     for volume_name in ${volumes}; do
         local volume_metadata
-        volume_metadata="$(get_volume_metadata_by_name "${cluster_config_json}" "${volume_name}")"
+        volume_metadata="$(get_file_volume_metadata_by_name "${cluster_config_json}" "${volume_name}")"
         create_artifacts_for_pv_az_file_volume "${deployment_json}" "${volume_metadata}" || true
+    done
+}
+
+function create_k8s_persistent_disk_volume_resource () {
+    local -r deployment_json="${1}"
+    local -r volume_name="${2}"
+    local -r volume_prefix="${3}"
+    local -r volume_spec="${4}"
+
+cat <<EOF
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: ${volume_prefix}-az-disks-${volume_name}-state-$(get_kube_namespace "${deployment_json}")-pv
+  labels:
+    usage: ${volume_prefix}-az-disks-${volume_name}-state-pv
+spec:
+  ${volume_spec}
+EOF
+}
+
+function create_k8s_persistent_disk_volume_claim () {
+    local -r deployment_json="${1}"
+    local -r volume_name="${2}"
+    local -r volume_prefix="${3}"
+    local -r volume_spec="${4}"
+    local volume_quota
+    volume_quota="$(jq -r -e '.capacity.storage' <<< "${volume_spec}")"
+
+cat <<EOF
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: ${volume_prefix}-az-disks-${volume_name}-state-$(get_kube_namespace "${deployment_json}")-pvc
+  namespace: $(get_kube_namespace "${deployment_json}")
+  labels:
+    usage: ${volume_prefix}-az-disks-${volume_name}-state-pvc
+spec:
+  resources:
+    requests:
+      storage: ${volume_quota}
+  accessModes:
+    - ReadWriteOnce
+  storageClassName: '${volume_name}-state'
+  volumeMode: 'Filesystem'
+  volumeName: ${volume_prefix}-az-disks-${volume_name}-state-$(get_kube_namespace "${deployment_json}")-pv
+EOF
+}
+
+function create_k8s_persistent_disk_volume () {
+    local -r deployment_json="${1}"
+    local -r volume_name="${2}"
+    local -r volume_prefix="${3}"
+    local -r volume_spec="${4}"
+
+    kubectl \
+        --context "$(get_kube_context "${deployment_json}")" \
+        --namespace "$(get_kube_namespace "${deployment_json}")" \
+        apply -f <( \
+          create_k8s_persistent_disk_volume_resource \
+            "${deployment_json}" \
+            "${volume_name}" \
+            "${volume_prefix}" \
+            "${volume_spec}" \
+        )
+    kubectl \
+        --context "$(get_kube_context "${deployment_json}")" \
+        --namespace "$(get_kube_namespace "${deployment_json}")" \
+        apply -f <( \
+          create_k8s_persistent_disk_volume_claim \
+            "${deployment_json}" \
+            "${volume_name}" \
+            "${volume_prefix}" \
+            "${volume_spec}" \
+        )
+}
+
+function get_disk_volume_metadata_by_name () {
+    local -r cluster_config_json="${1}"
+    local -r volume_name="${2}"
+    jq -r -e \
+      --arg volume_name "${volume_name}" \
+      '.target.paas.storage.azure_disks[] | select(.name == "\($volume_name)")' <<< "${cluster_config_json}"
+}
+
+function create_artifacts_for_pv_az_disk_volume () {
+    local -r deployment_json="${1}"
+    local -r volume_metadata="${2}"
+    local volume_name environment_metadata volume_quota volume_prefix sa_name
+    volume_name="$(jq -r -e '.name' <<< "${volume_metadata}")"
+    volume_quota="$(jq -r -e '.quota' <<< "${volume_metadata}")"
+    volume_prefix="$(get_pv_secret_namespace_prefix "${deployment_json}")"
+    volume_spec="$(jq -r -e '.k8s_volume_data' <<< "${volume_metadata}")"
+
+    create_k8s_persistent_disk_volume \
+      "${deployment_json}" \
+      "${volume_name}" \
+      "${volume_prefix}" \
+      "${volume_spec}" \
+    || true
+}
+
+function iterate_az_disk_volumes () {
+    local -r deployment_json="${1}"
+    local cluster_config_json volumes
+    cluster_config_json="$(get_cluster_config_json "${deployment_json}" )"
+    volumes="$(jq -r '.target.paas.storage.azure_disks[]? | .name // empty' <<< "${cluster_config_json}")"
+    for volume_name in ${volumes}; do
+        local volume_metadata
+        volume_metadata="$(get_disk_volume_metadata_by_name "${cluster_config_json}" "${volume_name}")"
+        create_artifacts_for_pv_az_disk_volume "${deployment_json}" "${volume_metadata}" || true
     done
 }
 
@@ -263,6 +418,7 @@ function deployment_helm_ensure_persistent_volumes () {
     local deployment_json
     deployment_json="$(get_deployment_json_by_name "${deployment_name}")"
     iterate_az_file_volumes "${deployment_json}"
+    iterate_az_disk_volumes "${deployment_json}"
 }
 
 deployment_helm_ensure_persistent_volumes "${@}"
