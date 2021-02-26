169e8032759b62fa5d49481606c01226bd654a1a
mystic knight <techguru@byiq.com>
2021-02-26 06:20:25 -1000

    add clone neuvector containers to bedrock pipeline scripts

--- tools/recipes/clone_neuvector.sh
+++ tools/recipes/clone_neuvector.sh
@@ -13,52 +13,83 @@ set -o pipefail
 
 # Environment Variables
 # ---------------------
-declare -rx DOCKER_TOKEN
-declare -rx DOCKER_USER
+declare -rx BEDROCK_INVOKED_DIR="${BEDROCK_INVOKED_DIR:-/src}"
+declare -rx DD_SECRET_VAULT="${DD_SECRET_VAULT:-/src}"
 
 # Arguments
 # ---------------------
 
-function repo_root () {
-    git rev-parse --show-toplevel
+function repo_root() {
+  git rev-parse --show-toplevel
 }
 
-function acr_registries () {
-  local -r environment="${1}"
-  printf '%sdevregistry\n' "${environment}"
-  printf '%sqaregistry\n' "${environment}"
-  printf '%sprodregistry\n' "${environment}"
+function get_vault_secret() {
+  local -r vault="${1}"
+  local -r secret_name="${2}"
+  az keyvault secret show \
+    --vault-name "${vault}" \
+    --name "${secret_name}" \
+    2>/dev/null |
+    jq -r '.value'
 }
 
-function acr_logins () {
-  local -r environment="${1}"
-  acr_registries "${environment}" | xargs -n 1 -r az acr login -n
+function get_docker_token() {
+  get_vault_secret "${DD_SECRET_VAULT}" 'docker-token'
 }
 
-function neuvector_containers () {
-    grep 'repository:' "$(repo_root)/configuration/k8s/charts/neuvector-helm/values.yaml" | sed -e 's|.* ||'
+function get_docker_user() {
+  get_vault_secret "${DD_SECRET_VAULT}" 'docker-user'
 }
 
-function clone_neuvector () {
-    local -r environment="${1}"
-    local -r originRepo=''
-    local containers
-    containers="$(neuvector_containers)"
-    printf 'containers [%s]\n' "${containers}"
-    acr_logins "${environment}"
-    echo "${DOCKER_TOKEN}" | docker login --username "${DOCKER_USER}" --password-stdin
-    for image in ${containers}; do
-        local originPath targetPath targetRepo
-        originPath="${originRepo}${image}"
-        docker pull "${originPath}"
-        #shellcheck disable=SC2043
-        for targetRepo in $(acr_registries "${environment}") ; do
-            local targetPath
-            targetPath="${targetRepo}.azurecr.io/${image}"
-            docker tag "${originPath}" "${targetPath}"
-            docker push "${targetPath}"
-        done
+function acr_registries() {
+  local -r app="${1}"
+  printf '%sdevregistry\n' "${app}"
+  printf '%sqaregistry\n' "${app}"
+  printf '%sprodregistry\n' "${app}"
+}
+
+function acr_logins() {
+  local -r app="${1}"
+  acr_registries "${app}" | xargs -n 1 -r az acr login -n
+}
+
+function neuvector_containers() {
+  # @@ TODO replace with neuvector chart managed by Bedrock
+  grep 'repository:' "${BEDROCK_INVOKED_DIR}/configuration/k8s/charts/neuvector-helm/values.yaml" | sed -e 's|.* ||'
+}
+
+function get_app() {
+  local -r deployment_json="${1}"
+  jq -r -e '.environment.app' <<<"${deployment_json}"
+}
+
+function get_deployment_json_by_name() {
+  local -r deployment_name="${1}"
+  "/bedrock/recipes/get_deployment_json_by_name.sh" "${deployment_name}"
+}
+
+function clone_neuvector() {
+  local -r deployment_name="${1}"
+  local deployment_json
+  deployment_json="$(get_deployment_json_by_name "${deployment_name}")"
+  local app containersa
+  app="$(get_app "${deployment_json}")"
+  containers="$(neuvector_containers)"
+  printf 'containers [%s]\n' "${containers}"
+  acr_logins "${app}"
+  get_docker_token | docker login --username "$(get_docker_user)" --password-stdin
+  for image in ${containers}; do
+    local originPath targetPath targetRepo
+    originPath="${image}"
+    docker pull "${originPath}" || continue
+    #shellcheck disable=SC2043
+    for targetRepo in $(acr_registries "${app}"); do
+      local targetPath
+      targetPath="${targetRepo}.azurecr.io/${image}"
+      docker tag "${originPath}" "${targetPath}" || continue
+      docker push "${targetPath}" || continue
     done
+  done
 }
 
 clone_neuvector "$@"
