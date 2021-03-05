26d167272c3581ed18d593020b4ba497bc81ea6a
mystic knight <techguru@byiq.com>
2021-03-04 16:43:54 -1000

    update tests for existing container registry

--- tools/paas/recipes/create_container_registry_if_needed.sh
+++ tools/paas/recipes/create_container_registry_if_needed.sh
@@ -43,59 +43,67 @@ declare -rx AZ_TRACE
 # ---------------------
 declare -rx CONTAINER_REGISTRY_NAME="${1}"
 
-function container_registry_name (){
-    echo "${CONTAINER_REGISTRY_NAME}"
+function container_registry_name() {
+  echo "${CONTAINER_REGISTRY_NAME}"
 }
 
-function repo_root () {
-    git rev-parse --show-toplevel
+function repo_root() {
+  git rev-parse --show-toplevel
 }
 
-function invoke_layer () {
-    local -r layer="${1}"
-    local -r target_recipe="${2}"
-    shift 2
-    "/bedrock/${layer}/recipes/${target_recipe}.sh" "$@"
+function invoke_layer() {
+  local -r layer="${1}"
+  local -r target_recipe="${2}"
+  shift 2
+  "/bedrock/${layer}/recipes/${target_recipe}.sh" "$@"
 }
 
-function target_config () {
-    printf '%s' "${TARGET_CONFIG}"
+function target_config() {
+  printf '%s' "${TARGET_CONFIG}"
 }
 
-function paas_configuration () {
-    yq eval-all --tojson "$(target_config)" | jq -r -e '.target.paas'
+function paas_configuration() {
+  yq eval-all --tojson "$(target_config)" | jq -r -e '.target.paas'
 }
 
-function acr_attr () {
-    local -r attr="${1}"
-    paas_configuration | jq -r -e ".container_registries[] | select(.name == \"$(container_registry_name)\") | .${attr}"
+function acr_attr() {
+  local -r attr="${1}"
+  paas_configuration | jq -r -e ".container_registries[] | select(.name == \"$(container_registry_name)\") | .${attr}"
 }
 
-function container_registry_resource_group () {
-    acr_attr 'resource_group'
+function container_registry_resource_group() {
+  acr_attr 'resource_group'
 }
 
-function fail_empty_set () {
-    grep -q '^'
+function fail_empty_set() {
+  grep -q '^'
 }
 
-function container_registry_already_exists () {
-    az acr show \
-        --name "$(container_registry_name)" \
-        --resource-group "$(container_registry_resource_group)" \
-        > /dev/null 2>&1
+function container_registry_already_exists_in_resource_group() {
+  az acr show \
+    --name "$(container_registry_name)" \
+    --resource-group "$(container_registry_resource_group)" \
+    >/dev/null 2>&1
 }
 
-function deploy_container_registry () {
-    $AZ_TRACE acr create \
-        --name "$(container_registry_name)" \
-        --resource-group "$(container_registry_resource_group)" \
-        --sku "$(acr_attr 'sku')" \
-        --admin-enabled  "$(acr_attr 'admin_enabled')"
+function container_registry_already_exists_in_dns() {
+  nc "$(container_registry_name).azurecr.io" 80
 }
 
-function create_container_registry_if_needed () {
-    container_registry_already_exists || deploy_container_registry
+function container_registry_already_exists() {
+  container_registry_already_exists_in_resource_group || container_registry_already_exists_in_dns
+}
+
+function deploy_container_registry() {
+  $AZ_TRACE acr create \
+    --name "$(container_registry_name)" \
+    --resource-group "$(container_registry_resource_group)" \
+    --sku "$(acr_attr 'sku')" \
+    --admin-enabled "$(acr_attr 'admin_enabled')"
+}
+
+function create_container_registry_if_needed() {
+  container_registry_already_exists || deploy_container_registry
 }
 
 create_container_registry_if_needed
